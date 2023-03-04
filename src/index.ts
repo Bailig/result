10b60380edd23e96150e25cfd55bf5ef2f8b0c09
Bailig <barrybailig@hotmail.com>
2023-03-04 15:50:15 +0300

    export typed error

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { fail, success } from "./result";
 export type { Success, Fail, Result, ResultMethods } from "./result";
+export { TypedError } from "./typed-error";
+export type { TypedErrorOptions, StringLiteral } from "./typed-error";
